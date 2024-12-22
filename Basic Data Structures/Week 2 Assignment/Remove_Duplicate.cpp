0ef164af0b23dd1cfc8e3b81dda59dbc8cd9c815
rakibhossain01 <rakibhossain157741@gmail.com>
2024-12-22 13:14:35 +0600

    commit

--- Basic Data Structures/Week 2 Assignment/Remove_Duplicate.cpp
+++ Basic Data Structures/Week 2 Assignment/Remove_Duplicate.cpp
@@ -16,7 +16,8 @@ void insert_at_tail(Node* &head, int val) {
     Node* newnode = new Node(val);
     if (head == NULL) {
         head = newnode;
-    } else {
+    } 
+    else {
         Node* tmp = head;
         while (tmp->next != NULL) {
             tmp = tmp->next;
